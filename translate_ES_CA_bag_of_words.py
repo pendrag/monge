9ae4b0d94a5f98da1855ae35edc93650450e841f
sjzafra <sjzafra@ujaen.es>
2018-02-09 12:36:09 +0100

    Traductor de bolsas de palabras al catalán

--- translate_ES_CA_bag_of_words.py
+++ translate_ES_CA_bag_of_words.py
@@ -1,49 +1,62 @@
-# uso: translate_ES_CA_bag_of_words.py
-
+# Usage: python3 translate_ES_CA_bag_of_words.py 
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
 '''
-@author Jiménez Zafra, Salud María 
-@author Plaza del Arco, Flor Miriam
-@author García Cumbreras, Miguel Ángel
+@author SINAI, Universidad de Jaén
 @created_at enero 2018
 '''
-
+import sys
+import codecs
+import argparse
 import os
 from googletrans import Translator
+import time
 
 
-def translate_module():
+def translate_bags_of_words_to_catalan(input_dir_es, output_dir_ca):
 
 	translator = Translator()
 
-
-	path_es = "../../sjzafra/bag_of_words"
-	path_cat = "/home/fmplaza/monge"
-	
-
-	for directory in os.listdir(path_es):
-		if not os.path.exists(directory):
-			os.makedirs(directory)
-		path_folder_es = os.path.join(path_es, directory)
+	for directory in os.listdir(input_dir_es):
+		output_dir_ca_model = os.path.join(output_dir_ca, directory)
+		if not os.path.exists(output_dir_ca_model):
+			os.makedirs(output_dir_ca_model)
+		path_folder_es = os.path.join(input_dir_es, directory)
 
 		for file_es in os.listdir(path_folder_es):
 			listWordsEs = []
-			file_read = open(str(os.path.join(path_folder_es, file_es)))
+			file_read = open(os.path.join(path_folder_es, file_es), 'r', encoding='latin-1')
 			for line in file_read:
 				listWordsEs.append(line)
-			file_cat = os.path.join(path_cat, directory, file_es)
-			if(not os.path.exists(file_cat)):
-				with open(file_cat, 'w') as output_file:
-					for word in listWordsEs:
-						print(word)
+			file_cat = os.path.join(output_dir_ca_model, file_es)
+			
+			with open(file_cat, 'w') as output_file:
+				for word in listWordsEs:
+					try:
 						word_cat = translator.translate(word, src='es',  dest='ca').text
 						line = word_cat + "\n"
 						output_file.write(line)
-				output_file.close()
+						time.sleep(5)
+					except:
+						time.sleep(10)
+						continue
 
+if __name__ == '__main__':
+	
+	# Set sys.stdout encoding	
+	sys.stdout = codecs.getwriter('utf8')(sys.stdout.buffer)
+	
+	# Parser for command line options
+	parser = argparse.ArgumentParser(description='Translate bag of words to catalan')
+	parser.add_argument('-i','--input_dir', help='Directory with the bags of words to be translated', required=True)
+	parser.add_argument('-o','--output_dir', help='Directory to save the translated bags of words', required=True)
+	
+	# Parse the command line options
+	args = parser.parse_args()
+	input_dir_es = args.input_dir
+	output_dir_ca = args.output_dir
 
-if __name__ == "__main__":
-
-	translate_module()
+	# Translate bags of words
+	translate_bags_of_words_to_catalan(input_dir_es, output_dir_ca)
 	
